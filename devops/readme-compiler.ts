c408366b3618aa7fa000dd6923ef1064d0043091
Rinto Jose <rintoj@gmail.com>
2021-05-13 20:18:18 +0530

    fix: Move icons without outline to the end of the table in readme

--- devops/readme-compiler.ts
+++ devops/readme-compiler.ts
@@ -8,6 +8,16 @@ function toOutline(iconName: string) {
   return `${iconName}-outline`
 }
 
+function sortByHasOutline(item1: any, item2: any) {
+  if (item1?.hasOutline && !item2?.hasOutline) {
+    return -1
+  } else if (!item1?.hasOutline && item2?.hasOutline) {
+    return 1
+  } else {
+    return 0
+  }
+}
+
 export function compileReadme(pattern = 'src/*-icon.tsx') {
   const files = sync(pattern)
   const iconNames = files.map(file => basename(file).replace('-icon', '').replace('.tsx', ''))
@@ -27,6 +37,7 @@ export function compileReadme(pattern = 'src/*-icon.tsx') {
         outlineComponentName: hasOutline ? toClassName(outlineIconName) : '-',
       }
     })
+    .sort(sortByHasOutline)
   const content = readFileSync('./devops/README.md.tpl', 'utf8')
   const template = compile(content)
   writeFileSync('./README.md', template({ icons }))
